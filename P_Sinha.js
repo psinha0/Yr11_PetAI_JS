998fed9f79d0f86a86aa5c9417b466d87db2534e
Prathmesh Sinha <prathmeshsinha@Prathmeshs-MacBook-Air.local>
2020-07-30 16:02:21 +1000

    Init

---
+++ P_Sinha.js
@@ -0,0 +1,114 @@
+var myGamePiece;
+var healthBar;
+var fillerImage;
+var mainGems = 1000;
+var petName = 0;
+var upcomingEvent = 0;
+var newRectangle;
+
+function startCanvas() {
+  healthBar = new component(400, 30, "#f7b267", 500, 500, "scoreBar", 1);
+  fillerImage = new component(500, 500, "sloth.png", 0, 0, "image");
+  myCanvasArea.start();
+}
+
+var myCanvasArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+    this.canvas.width = 0.9*screen.width;
+    this.canvas.height = 0.8*screen.height;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.frameNo = 0;
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  clear : function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  stop : function() {
+    clearInterval(this.interval);
+  }
+}
+
+function component(width, height, color, x, y, type, rate=0) {
+  this.type = type;
+  if (type == "image" || type == "background") {
+    this.image = new Image();
+    this.image.src = color;
+  }
+  this.width = width;
+  var scoreWidth = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.rate = rate;
+  this.update = function() {
+    ctx = myCanvasArea.context;
+    if (type == "image" || type == "background") {
+        ctx.drawImage(this.image,
+        this.x,
+        this.y,
+        this.width, this.height);
+    if (type == "background") {
+      ctx.drawImage(this.image,
+        this.x + this.width,
+        this.y,
+        this.width, this.height);
+      }
+    }
+    else if (type == "scoreBar") {
+      if (scoreWidth > 0) {
+        scoreWidth = scoreWidth - this.rate;
+      }
+      ctx.fillRect(this.x, this.y, scoreWidth, this.height);
+    }
+    else {
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+}
+
+var emotions = {
+  affection : 0,
+  loneliness: 0,
+  call : function(call) {
+    if (call.includes("aff")) {
+      return this.affection;
+    }
+    else if (call.includes("lone")) {
+      return this.loneliness;
+    }
+  }
+}
+
+var reactions = {
+  hungry: petName + " is starving and needs to be fed immediately.",
+  // Ben helped me with hungry reaction. Credits to Benjamin David Avery [24/07/2020].
+  sad: petName + " is depressed for unknown reasons.",
+  //lowhealthReaction: petName + " has very low health! Take it to a Pokecenter right now!",
+  angry: petName + " is angry at you for unknown reasons.",
+  dislike: petName + " doesn't seem to be fond of you.",
+  indifferent: petName + " seems indifferent about you.",
+  happy: petName + " feels happy!",
+  excited: petName + " seems extremely excited about " + upcomingEvent,
+  cheerful: petName + " assures you that they are here for you!",
+  love: petName + " seems to seriously adore you!"
+}
+
+
+
+function updateEmotion(personality) {
+  if (emotions.affection > 80 && emotions.loneliness < 30) {
+    // Derive an algorithm to accurately update emotion.
+    return reactions.love;
+  }
+}
+
+function updateGameArea() {
+  myCanvasArea.clear();
+  var myComponents = [healthBar, fillerImage];
+  for (const item of myComponents) {
+    item.update();
+  }
+}
