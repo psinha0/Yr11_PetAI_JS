bd390b612f288810e162f304b6a7c863ec5f1737
Prathmesh Sinha <prathmeshsinha@Prathmeshs-MacBook-Air.local>
2020-08-10 14:26:03 +1000

    Added buttons

--- P_Sinha.js
+++ P_Sinha.js
@@ -4,11 +4,18 @@ var fillerImage;
 var mainGems = 1000;
 var petName = 0;
 var upcomingEvent = 0;
-var newRectangle;
+var rightSideCanvas;
+var botSideCanvas;
 
 function startCanvas() {
-  healthBar = new component(400, 30, "#f7b267", 500, 500, "scoreBar", 1);
-  fillerImage = new component(500, 500, "sloth.png", 0, 0, "image");
+  fillerImage = new component(0.4*screen.width, 0.5*screen.height, "sloth.png", 0.13*screen.width, 0.105*screen.height, "image");
+  rightSideCanvas = new component(0.22*screen.width, 0.8*screen.height, "#20B2AA", 0.68*screen.width, 0, "color");
+  botSideCanvas = new component(0.68*screen.width, 0.1*screen.height, "#FFDAB9", 0, 0.7*screen.height, "color");
+  buttonOne = new createButton(0.755*screen.width, 0.1*screen.height, "alert('hi')");
+  buttonTwo = new createButton(0.755*screen.width, 0.2*screen.height, "alert('byebye')");
+  buttonThree = new createButton(0.755*screen.width, 0.3*screen.height, "alert('no i do not')");
+  healthBar = new component(400, 30, "#F7B267", 500, 500, "scoreBar", 1);
+  // Credits to Benjamin "Sean's Schlong's Long" Avery and Sean "Stopwatch" Hou
   myCanvasArea.start();
 }
 
@@ -17,6 +24,7 @@ var myCanvasArea = {
   start : function() {
     this.canvas.width = 0.9*screen.width;
     this.canvas.height = 0.8*screen.height;
+    this.canvas.classList.add("mainCanvas");
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.frameNo = 0;
@@ -28,6 +36,21 @@ var myCanvasArea = {
   stop : function() {
     clearInterval(this.interval);
   }
+}
+
+function createButton(x, y, output) {
+  this.buttonCanvas = document.createElement("canvas");
+  this.buttonCanvas.width = 0.17*screen.width;
+  this.buttonCanvas.height = 0.06*screen.height;
+  this.buttonCanvas.id = "buttonCanvas";
+  this.context = this.buttonCanvas.getContext("2d");
+  document.body.insertBefore(this.buttonCanvas, document.body.childNodes[0]);
+  var buttonStyling = document.getElementById("buttonCanvas");
+  buttonStyling.style.left = x + "px";
+  buttonStyling.style.top = y + "px";
+  this.buttonCanvas.addEventListener('click', function(event) {
+    eval(output);
+  }, false);
 }
 
 function component(width, height, color, x, y, type, rate=0) {
@@ -44,6 +67,7 @@ function component(width, height, color, x, y, type, rate=0) {
   this.rate = rate;
   this.update = function() {
     ctx = myCanvasArea.context;
+    // ctx.fillText(updateEmotion(personality), x, y);
     if (type == "image" || type == "background") {
         ctx.drawImage(this.image,
         this.x,
@@ -57,6 +81,7 @@ function component(width, height, color, x, y, type, rate=0) {
       }
     }
     else if (type == "scoreBar") {
+      ctx.fillStyle = color;
       if (scoreWidth > 0) {
         scoreWidth = scoreWidth - this.rate;
       }
@@ -66,6 +91,10 @@ function component(width, height, color, x, y, type, rate=0) {
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    ctx.font = "30px Palatino Linotype";
+    ctx.fillStyle = "red";
+    ctx.textAlign = "center";
+    ctx.fillText(reactions.love, 0.34*screen.width, 0.77*screen.height);
   }
 }
 
@@ -107,7 +136,7 @@ function updateEmotion(personality) {
 
 function updateGameArea() {
   myCanvasArea.clear();
-  var myComponents = [healthBar, fillerImage];
+  var myComponents = [healthBar, fillerImage, rightSideCanvas, botSideCanvas];
   for (const item of myComponents) {
     item.update();
   }
