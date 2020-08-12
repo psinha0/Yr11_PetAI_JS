00e0e647e1bf7dafe56e8dfdc302b3c73bfce079
psinha0 <sinha.prathmesh1604@gmail.com>
2020-08-13 00:03:43 +1000

    Added jquery and cookies

--- P_Sinha.js
+++ P_Sinha.js
@@ -3,18 +3,20 @@ var healthBar;
 var fillerImage;
 var mainGems = 1000;
 var petName = 0;
-var upcomingEvent = 0;
+var upcomingEvent;
 var rightSideCanvas;
 var botSideCanvas;
+var timePassed;
 
 function startCanvas() {
   fillerImage = new component(0.4*screen.width, 0.5*screen.height, "sloth.png", 0.13*screen.width, 0.105*screen.height, "image");
   rightSideCanvas = new component(0.22*screen.width, 0.8*screen.height, "#20B2AA", 0.68*screen.width, 0, "color");
   botSideCanvas = new component(0.68*screen.width, 0.1*screen.height, "#FFDAB9", 0, 0.7*screen.height, "color");
-  buttonOne = new createButton(0.755*screen.width, 0.1*screen.height, "alert('hi')");
-  buttonTwo = new createButton(0.755*screen.width, 0.2*screen.height, "alert('byebye')");
-  buttonThree = new createButton(0.755*screen.width, 0.3*screen.height, "alert('no i do not')");
-  healthBar = new component(400, 30, "#F7B267", 500, 500, "scoreBar", 1);
+  buttonOne = new createButton(0.755*screen.width, 0.1*screen.height, "alert(timePassed);");
+  buttonTwo = new createButton(0.755*screen.width, 0.2*screen.height, "alert(document.cookie);");
+  buttonThree = new createButton(0.755*screen.width, 0.3*screen.height, "$('.mainCanvas').hide();");
+  // Currently, the hungerBar reduces to zero in a minute.
+  hungerBar = new component(0.177*screen.width, 0.04*screen.height, "#F7B267", 0.7*screen.width, 0.5*screen.height, "scoreBar", (20 / (60 * 1000)) * 0.177*screen.width);
   // Credits to Benjamin "Sean's Schlong's Long" Avery and Sean "Stopwatch" Hou
   myCanvasArea.start();
 }
@@ -81,6 +83,9 @@ function component(width, height, color, x, y, type, rate=0) {
       }
     }
     else if (type == "scoreBar") {
+      // Rate decreases a given rate variable from the width every 20ms.
+      // In order to decrease the entire thing in a second, let the rate be
+      // 20/1000 of the total width (as 1000ms = 1 second).
       ctx.fillStyle = color;
       if (scoreWidth > 0) {
         scoreWidth = scoreWidth - this.rate;
@@ -125,8 +130,6 @@ var reactions = {
   love: petName + " seems to seriously adore you!"
 }
 
-
-
 function updateEmotion(personality) {
   if (emotions.affection > 80 && emotions.loneliness < 30) {
     // Derive an algorithm to accurately update emotion.
@@ -134,9 +137,49 @@ function updateEmotion(personality) {
   }
 }
 
+// Credits to Mandeep Janjua for setCookie, getCookie and eraseCookie
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
+$( document ).ready(function(event) {
+  var currentDate = new Date().getTime();
+  var previousDate = readCookie("lastSeen");
+  timePassed = Math.floor((currentDate-previousDate / (1000 * 60 * 60)) % 24);
+});
+
+window.addEventListener('beforeunload', function (event) {
+  var currentDate = new Date().getTime();
+  console.log(currentDate);
+  eraseCookie("lastSeen");
+  createCookie("lastSeen", currentDate, 9999);
+});
+
 function updateGameArea() {
   myCanvasArea.clear();
-  var myComponents = [healthBar, fillerImage, rightSideCanvas, botSideCanvas];
+  var myComponents = [fillerImage, rightSideCanvas, botSideCanvas, hungerBar];
   for (const item of myComponents) {
     item.update();
   }
