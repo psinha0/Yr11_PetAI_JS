53544beea090470a4caf73edc6fed56efeb22578
psinha0 <sinha.prathmesh1604@gmail.com>
2020-08-20 09:42:53 +1000

    Added a food shop menu + alertifyjs

--- P_Sinha.js
+++ P_Sinha.js
@@ -4,14 +4,26 @@ var upcomingEvent;
 var timePassed;
 var pushHunger = 0;
 var mainImageVisibility = true;
+var foodShopMenu = false;
 
 function startCanvas() {
   fillerImage = new component(0.4*screen.width, 0.5*screen.height, "sloth.png", 0.13*screen.width, 0.105*screen.height, "image", 0, "mainImageVisibility");
+
+  catFoodOne = new component(0.07*screen.width, 0.10*screen.height, "cat_food.jpg", 0.08*screen.width, 0.1*screen.height, "image", 0, "foodShopMenu");
+  catFoodTwo = new component(0.07*screen.width, 0.10*screen.height, "cat_food.jpg", 0.19*screen.width, 0.1*screen.height, "image", 0, "foodShopMenu");
+  catFoodThree = new component(0.07*screen.width, 0.10*screen.height, "cat_food.jpg", 0.30*screen.width, 0.1*screen.height, "image", 0, "foodShopMenu");
+  catFoodFour = new component(0.07*screen.width, 0.10*screen.height, "cat_food.jpg", 0.41*screen.width, 0.1*screen.height, "image", 0, "foodShopMenu");
+  catFoodFive = new component(0.07*screen.width, 0.10*screen.height, "cat_food.jpg", 0.52*screen.width, 0.1*screen.height, "image", 0, "foodShopMenu");
+
+  foodOneBuy = new createButton(0.07*screen.width, 0.03*screen.height, 0.13*screen.width, 0.25*screen.height, "catFoods.catFoodOne += 1;", "buyButtonOne", "buyButton");
+
   rightSideCanvas = new component(0.22*screen.width, 0.8*screen.height, "#20B2AA", 0.68*screen.width, 0, "color");
   botSideCanvas = new component(0.68*screen.width, 0.1*screen.height, "#FFDAB9", 0, 0.7*screen.height, "color");
-  buttonOne = new createButton(0.755*screen.width, 0.1*screen.height, "console.log(mainImageVisibility);");
-  buttonTwo = new createButton(0.755*screen.width, 0.2*screen.height, "pushHunger = 10;");
-  buttonThree = new createButton(0.755*screen.width, 0.3*screen.height, "mainImageVisibility = false;");
+
+  buttonOne = new createButton(0.17*screen.width, 0.06*screen.height, 0.755*screen.width, 0.1*screen.height, "alertify.alert('hi')", "buttonOne", "menuButton");
+  buttonTwo = new createButton(0.17*screen.width, 0.06*screen.height, 0.755*screen.width, 0.2*screen.height, "pushHunger = 10;", "buttonTwo", "menuButton");
+  buttonThree = new createButton(0.17*screen.width, 0.06*screen.height, 0.755*screen.width, 0.3*screen.height, "openShop();", "buttonThree", "menuButton");
+  buttonFour = new createButton(0.17*screen.width, 0.06*screen.height, 0.755*screen.width, 0.4*screen.height, "alert(catFoods.catFoodOne);", "buttonFour", "menuButton");
   // Currently, the hungerBar reduces to zero in a minute.
   hungerBar = new component(0.177*screen.width, 0.04*screen.height, "#F7B267", 0.7*screen.width, 0.5*screen.height, "scoreBar", (20 / (60 * 1000)) * 0.177*screen.width);
   // Credits to Benjamin "Sean's Schlong's Long" Avery and Sean "Stopwatch" Hou
@@ -37,18 +49,22 @@ var myCanvasArea = {
   }
 }
 
-function createButton(x, y, output) {
+function createButton(width, height, x, y, output, id, buttonClass) {
   this.buttonCanvas = document.createElement("canvas");
-  this.buttonCanvas.width = 0.17*screen.width;
-  this.buttonCanvas.height = 0.06*screen.height;
-  this.buttonCanvas.id = "buttonCanvas";
+  this.buttonCanvas.width = width;
+  this.buttonCanvas.height = height;
+  this.buttonCanvas.id = id;
+  this.buttonCanvas.classList.add(buttonClass);
+  // The code above assigns the width, height, id and class
   this.context = this.buttonCanvas.getContext("2d");
   document.body.insertBefore(this.buttonCanvas, document.body.childNodes[0]);
-  var buttonStyling = document.getElementById("buttonCanvas");
+  // ^ Appends the <canvas> tag below the <body> tag in html ^
+  var buttonStyling = document.getElementById(id);
   buttonStyling.style.left = x + "px";
   buttonStyling.style.top = y + "px";
+  // The code below adds an event which runs upon clicking the canvas
   this.buttonCanvas.addEventListener('click', function(event) {
-    eval(output);
+    eval(output); // Runs the output code
   }, false);
 }
 
@@ -69,7 +85,7 @@ function component(width, height, color, x, y, type, rate=0, visibility=true) {
     // ctx.fillText(updateEmotion(personality), x, y);
     if (type == "image" || type == "background") {
         if (eval(this.visibility) == true) {
-          this.image.src = color;
+          this.image.src = "images/" + color;
         }
         else {
           this.image.src = "";
@@ -94,7 +110,7 @@ function component(width, height, color, x, y, type, rate=0, visibility=true) {
         scoreWidth = scoreWidth - this.rate;
       }
       ctx.fillStyle = "#3B444B";
-      ctx.fillRect(this.x, this.y, this.width + 10, this.height);
+      ctx.fillRect(this.x, this.y, this.width + 5, this.height);
       ctx.fillStyle = color;
       ctx.fillRect(this.x, this.y, scoreWidth, this.height);
     }
@@ -143,6 +159,23 @@ function updateEmotion(personality) {
   }
 }
 
+var catFoods = {
+  catFoodOne: 0,
+  catFoodTwo: 0,
+  catFoodThree: 0,
+  catFoodFour: 0,
+  catFoodFive: 0
+}
+
+function openShop() {
+  mainImageVisibility = false;
+  foodShopMenu = true;
+  var buyButton = document.getElementsByClassName('buyButton');
+  for (var i = 0; i < buyButton.length; i++) {
+    buyButton[i].style.visibility = 'visible';
+  }
+}
+
 // Credits to Mandeep Janjua for setCookie, getCookie and eraseCookie
 
 function createCookie(name,value,days) {
@@ -175,7 +208,7 @@ $( document ).ready(function(event) {
   var previousDate = readCookie("lastSeen");
   // timePassed = ((currentDate-previousDate) / (1000 * 60 * 60)).toFixed(1);
   timePassed = ((currentDate-previousDate) / (1000)).toFixed(1);
-  alert("You have been gone " + timePassed + " seconds!")
+  // alert("You have been gone " + timePassed + " seconds!")
 });
 
 window.addEventListener('beforeunload', function (event) {
@@ -187,7 +220,7 @@ window.addEventListener('beforeunload', function (event) {
 
 function updateGameArea() {
   myCanvasArea.clear();
-  var myComponents = [fillerImage, rightSideCanvas, botSideCanvas, hungerBar];
+  var myComponents = [fillerImage, rightSideCanvas, botSideCanvas, hungerBar, catFoodOne, catFoodTwo, catFoodThree, catFoodFour, catFoodFive];
   for (const item of myComponents) {
     item.update();
   }
